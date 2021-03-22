29e5c1f4938d42e4e1c08fd4a2ff573456dc2d2e
Andrew Mackenzie <andrew@mackenzie-serres.net>
2021-03-22 19:00:31 +0100

    Implement OrderedSetLattice

--- cli/src/lib/lattices/core_lattices.rs
+++ cli/src/lib/lattices/core_lattices.rs
@@ -3,6 +3,9 @@ use std::hash::Hash;
 use std::iter::Extend;
 use std::ops::{Add, Sub};
 
+extern crate linked_hash_set;
+use linked_hash_set::LinkedHashSet;
+
 trait Lattice {
     type A;
 
@@ -102,8 +105,7 @@ where
 
     /// Insert a new element into the `SetLattice`
     pub fn insert(&mut self, l: T) {
-        let set = &mut self.0;
-        set.insert(l);
+        self.0.insert(l);
     }
 
     /// Calculate a new `SetLattice` that is the intersection of `Self` with another `SetLattice`
@@ -128,46 +130,76 @@ where
     // }
 }
 
-// template <typename T>
-// class OrderedSetLattice : public Lattice<ordered_set<T>> {
-//  protected:
-//   void do_merge(const ordered_set<T> &e) {
-//     for (const T &elem : e) {
-//       this->element.insert(elem);
-//     }
-//   }
-//
-//  public:
-//   OrderedSetLattice() : Lattice<ordered_set<T>>(ordered_set<T>()) {}
-//
-//   OrderedSetLattice(const ordered_set<T> &e) : Lattice<ordered_set<T>>(e) {}
-//
-//   MaxLattice<unsigned> size() const { return this->element.size(); }
-//
-//   void insert(T e) { this->element.insert(std::move(e)); }
-//
-//   OrderedSetLattice<T> intersect(ordered_set<T> s) const {
-//     ordered_set<T> res;
-//
-//     for (const T &that_elem : s) {
-//       for (const T &this_elem : this->element) {
-//         if (this_elem == that_elem) res.insert(this_elem);
-//       }
-//     }
-//
-//     return OrderedSetLattice<T>(res);
-//   }
-//
-//   OrderedSetLattice<T> project(bool (*f)(T)) const {
-//     ordered_set<T> res;
-//
-//     for (const T &elem : this->element) {
-//       if (f(elem)) res.insert(elem);
-//     }
-//
-//     return OrderedSetLattice<T>(res);
-//   }
-// };
+/// A `OrderedSetLattice` containing an ordered set of elements of type `T`
+#[derive(Default, Debug)]
+struct OrderedSetLattice<T>(LinkedHashSet<T>)
+where
+    T: Eq + Hash;
+
+impl<T> Lattice for OrderedSetLattice<T>
+where
+    T: Eq + Hash + Clone,
+{
+    type A = T;
+
+    fn do_merge(&mut self, l: &OrderedSetLattice<T>) {
+        let set = &mut self.0;
+        let other_set = &l.0;
+        set.extend(other_set.into_iter().map(Clone::clone));
+    }
+}
+
+impl<T> PartialEq for OrderedSetLattice<T>
+where
+    T: Eq + Hash,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl<T> OrderedSetLattice<T>
+where
+    T: Eq + Hash + Clone,
+{
+    /// Create a new, empty, `OrderedSetLattice`
+    pub fn new() -> Self {
+        OrderedSetLattice(LinkedHashSet::new())
+    }
+
+    /// Return the number of elements in the `SetLattice` as a `MaxLattice<usize>`
+    pub fn size(&self) -> MaxLattice<usize> {
+        MaxLattice(self.0.len())
+    }
+
+    /// Insert a new element into the `SetLattice`
+    pub fn insert(&mut self, l: T) {
+        self.0.insert(l);
+    }
+
+    /// Calculate a new `OrderedSetLattice` that is the intersection of `Self` with another `OrderedSetLattice`
+    pub fn intersect(&self, l: &OrderedSetLattice<T>) -> OrderedSetLattice<T> {
+        let other_set = &l.0;
+        let intersection = self.0.intersection(other_set);
+        let mut new_set: LinkedHashSet<T> = LinkedHashSet::new();
+        for element in intersection {
+            new_set.insert(element.clone());
+        }
+        Self(new_set)
+    }
+
+    //   OrderedSetLattice<T> project(bool (*f)(T)) const {
+    //     ordered_set<T> res;
+    //
+    //     for (const T &elem : this->element) {
+    //       if (f(elem)) res.insert(elem);
+    //     }
+    //
+    //     return OrderedSetLattice<T>(res);
+    //   }
+    // };
+}
+
 //
 // template <typename K, typename V>
 // class MapLattice : public Lattice<map<K, V>> {
@@ -244,162 +276,256 @@ where
 
 #[cfg(test)]
 mod test {
-    use crate::lattices::core_lattices::{BoolLattice, Lattice, MaxLattice, SetLattice};
-    use std::collections::HashSet;
+    mod bool_lattice {
+        use crate::lattices::core_lattices::{BoolLattice, Lattice};
+        #[test]
+        fn default_bool_lattice() {
+            let bool_lattice = BoolLattice::default();
+            assert_eq!(bool_lattice.0, false);
+        }
 
-    #[test]
-    fn default_bool_lattice() {
-        let bool_lattice = BoolLattice::default();
-        assert_eq!(bool_lattice.0, false);
+        #[test]
+        fn create_false_bool_lattice() {
+            let bool_lattice = BoolLattice(false);
+            assert_eq!(bool_lattice.0, false);
+        }
+
+        #[test]
+        fn create_true_bool_lattice() {
+            let bool_lattice = BoolLattice(true);
+            assert_eq!(bool_lattice.0, true);
+        }
+
+        #[test]
+        fn merge_true_false_bool_lattice() {
+            let mut bool_lattice = BoolLattice(false);
+            bool_lattice.do_merge(&BoolLattice(true));
+            assert_eq!(bool_lattice.0, true)
+        }
     }
 
-    #[test]
-    fn create_false_bool_lattice() {
-        let bool_lattice = BoolLattice(false);
-        assert_eq!(bool_lattice.0, false);
+    mod max_lattice {
+        use crate::lattices::core_lattices::{Lattice, MaxLattice};
+
+        #[test]
+        fn default_max_u32_lattice() {
+            let lattice = MaxLattice::<u32>::default();
+            assert_eq!(lattice.0, 0);
+        }
+
+        #[test]
+        fn merge_max_u32_lattice() {
+            let mut low_lattice = MaxLattice::<u32>(1);
+            let high_lattice = MaxLattice::<u32>(42);
+            low_lattice.do_merge(&high_lattice);
+            assert_eq!(low_lattice.0, 42)
+        }
+
+        #[test]
+        fn add_max_u32_lattice() {
+            let low_lattice = MaxLattice::<u32>(1);
+            let high_lattice = MaxLattice::<u32>(42);
+            assert_eq!(low_lattice + high_lattice, MaxLattice::<u32>(43))
+        }
+
+        #[test]
+        fn sub_max_u32_lattice() {
+            let low_lattice = MaxLattice::<u32>(1);
+            let high_lattice = MaxLattice::<u32>(42);
+            assert_eq!(high_lattice - low_lattice, MaxLattice::<u32>(41))
+        }
+
+        #[test]
+        fn sub_max_u64_lattice() {
+            let low_lattice = MaxLattice::<u64>(100);
+            let high_lattice = MaxLattice::<u64>(142);
+            assert_eq!(high_lattice - low_lattice, MaxLattice::<u64>(42))
+        }
     }
 
-    #[test]
-    fn create_true_bool_lattice() {
-        let bool_lattice = BoolLattice(true);
-        assert_eq!(bool_lattice.0, true);
+    mod set_lattice {
+        use crate::lattices::core_lattices::{Lattice, MaxLattice, SetLattice};
+        use std::collections::HashSet;
+
+        #[test]
+        fn size_of_empty_set() {
+            let set_lattice: SetLattice<usize> = SetLattice::new();
+            assert_eq!(set_lattice.size(), MaxLattice(0));
+        }
+
+        #[test]
+        fn size_of_set() {
+            let mut set: HashSet<usize> = HashSet::new();
+            set.insert(1);
+            set.insert(42);
+            let set_lattice = SetLattice(set);
+            assert_eq!(set_lattice.size(), MaxLattice(2));
+        }
+
+        #[test]
+        fn insert_to_set() {
+            let mut set_lattice: SetLattice<usize> = SetLattice::new();
+            set_lattice.insert(1);
+            set_lattice.insert(42);
+            assert_eq!(set_lattice.size(), MaxLattice(2));
+        }
+
+        #[test]
+        fn merge_two_sets() {
+            let mut set_lattice1: SetLattice<usize> = SetLattice::new();
+            // let mut set_lattice1 = SetLattice(set1);
+            set_lattice1.insert(1);
+            set_lattice1.insert(42);
+
+            let set2: HashSet<usize> = HashSet::new();
+            let mut set_lattice2 = SetLattice(set2);
+            set_lattice2.insert(3);
+            set_lattice2.insert(100);
+
+            set_lattice1.do_merge(&set_lattice2);
+
+            assert_eq!(set_lattice1.size(), MaxLattice(4));
+        }
+
+        #[test]
+        fn merge_two_intersecting_sets() {
+            let mut set_lattice1: SetLattice<usize> = SetLattice::new();
+            set_lattice1.insert(1);
+            set_lattice1.insert(42);
+
+            let mut set_lattice2: SetLattice<usize> = SetLattice::new();
+            set_lattice2.insert(1);
+            set_lattice2.insert(100);
+
+            set_lattice1.do_merge(&set_lattice2);
+
+            assert_eq!(set_lattice1.size(), MaxLattice(3));
+        }
+
+        #[test]
+        fn intersection_of_two_sets() {
+            let mut set_lattice1: SetLattice<usize> = SetLattice::new();
+            set_lattice1.insert(1);
+            set_lattice1.insert(42);
+
+            let mut set_lattice2: SetLattice<usize> = SetLattice::new();
+            set_lattice2.insert(1);
+            set_lattice2.insert(100);
+
+            assert_eq!(set_lattice1.intersect(&set_lattice2).size(), MaxLattice(1));
+        }
+
+        #[test]
+        fn equality_of_two_sets() {
+            let mut set_lattice1: SetLattice<usize> = SetLattice::new();
+            set_lattice1.insert(1);
+            set_lattice1.insert(42);
+
+            let mut set_lattice2: SetLattice<usize> = SetLattice::new();
+            set_lattice2.insert(1);
+            set_lattice2.insert(42);
+
+            assert_eq!(set_lattice1, set_lattice2);
+        }
     }
 
-    #[test]
-    fn merge_true_false_bool_lattice() {
-        let mut bool_lattice = BoolLattice(false);
-        bool_lattice.do_merge(&BoolLattice(true));
-        assert_eq!(bool_lattice.0, true)
-    }
+    mod ordered_set_lattice {
+        use super::super::linked_hash_set::LinkedHashSet;
+        use crate::lattices::core_lattices::{Lattice, MaxLattice, OrderedSetLattice};
 
-    #[test]
-    fn default_max_u32_lattice() {
-        let lattice = MaxLattice::<u32>::default();
-        assert_eq!(lattice.0, 0);
-    }
+        #[test]
+        fn size_of_empty_ordered_set() {
+            let set_lattice: OrderedSetLattice<usize> = OrderedSetLattice::new();
+            assert_eq!(set_lattice.size(), MaxLattice(0));
+        }
 
-    #[test]
-    fn merge_max_u32_lattice() {
-        let mut low_lattice = MaxLattice::<u32>(1);
-        let high_lattice = MaxLattice::<u32>(42);
-        low_lattice.do_merge(&high_lattice);
-        assert_eq!(low_lattice.0, 42)
-    }
+        #[test]
+        fn size_of_ordered_set() {
+            let mut set: LinkedHashSet<usize> = LinkedHashSet::new();
+            set.insert(1);
+            set.insert(42);
+            let set_lattice = OrderedSetLattice(set);
+            assert_eq!(set_lattice.size(), MaxLattice(2));
+        }
 
-    #[test]
-    fn add_max_u32_lattice() {
-        let low_lattice = MaxLattice::<u32>(1);
-        let high_lattice = MaxLattice::<u32>(42);
-        assert_eq!(low_lattice + high_lattice, MaxLattice::<u32>(43))
-    }
+        #[test]
+        fn insert_to_ordered_set() {
+            let mut set_lattice: OrderedSetLattice<usize> = OrderedSetLattice::new();
+            set_lattice.insert(1);
+            set_lattice.insert(42);
+            assert_eq!(set_lattice.size(), MaxLattice(2));
+        }
 
-    #[test]
-    fn sub_max_u32_lattice() {
-        let low_lattice = MaxLattice::<u32>(1);
-        let high_lattice = MaxLattice::<u32>(42);
-        assert_eq!(high_lattice - low_lattice, MaxLattice::<u32>(41))
-    }
+        #[test]
+        fn merge_two_ordered_sets() {
+            let mut set_lattice1: OrderedSetLattice<usize> = OrderedSetLattice::new();
+            // let mut set_lattice1 = SetLattice(set1);
+            set_lattice1.insert(1);
+            set_lattice1.insert(42);
 
-    #[test]
-    fn sub_max_u64_lattice() {
-        let low_lattice = MaxLattice::<u64>(100);
-        let high_lattice = MaxLattice::<u64>(142);
-        assert_eq!(high_lattice - low_lattice, MaxLattice::<u64>(42))
-    }
+            let set2: LinkedHashSet<usize> = LinkedHashSet::new();
+            let mut set_lattice2 = OrderedSetLattice(set2);
+            set_lattice2.insert(3);
+            set_lattice2.insert(100);
 
-    #[test]
-    fn size_of_empty_set() {
-        // let set: HashSet<usize> = HashSet::new();
-        let set_lattice: SetLattice<usize> = SetLattice::new();
-        assert_eq!(set_lattice.size(), MaxLattice(0));
-    }
+            set_lattice1.do_merge(&set_lattice2);
 
-    #[test]
-    fn size_of_set() {
-        let mut set: HashSet<usize> = HashSet::new();
-        set.insert(1);
-        set.insert(42);
-        let set_lattice = SetLattice(set);
-        assert_eq!(set_lattice.size(), MaxLattice(2));
-    }
+            assert_eq!(set_lattice1.size(), MaxLattice(4));
+        }
 
-    #[test]
-    fn insert_to_set() {
-        // let set: HashSet<usize> = HashSet::new();
-        let mut set_lattice: SetLattice<usize> = SetLattice::new();
-        set_lattice.insert(1);
-        set_lattice.insert(42);
-        assert_eq!(set_lattice.size(), MaxLattice(2));
-    }
+        #[test]
+        fn merge_two_intersecting_ordered_sets() {
+            let mut set_lattice1: OrderedSetLattice<usize> = OrderedSetLattice::new();
+            set_lattice1.insert(1);
+            set_lattice1.insert(42);
 
-    #[test]
-    fn merge_two_sets() {
-        // let set1: HashSet<usize> = HashSet::new();
-        let mut set_lattice1: SetLattice<usize> = SetLattice::new();
-        // let mut set_lattice1 = SetLattice(set1);
-        set_lattice1.insert(1);
-        set_lattice1.insert(42);
+            let mut set_lattice2: OrderedSetLattice<usize> = OrderedSetLattice::new();
+            set_lattice2.insert(1);
+            set_lattice2.insert(100);
 
-        let set2: HashSet<usize> = HashSet::new();
-        let mut set_lattice2 = SetLattice(set2);
-        set_lattice2.insert(3);
-        set_lattice2.insert(100);
+            set_lattice1.do_merge(&set_lattice2);
 
-        set_lattice1.do_merge(&set_lattice2);
+            assert_eq!(set_lattice1.size(), MaxLattice(3));
+        }
 
-        assert_eq!(set_lattice1.size(), MaxLattice(4));
-    }
+        #[test]
+        fn intersection_of_two_ordered_sets() {
+            let mut set_lattice1: OrderedSetLattice<usize> = OrderedSetLattice::new();
+            set_lattice1.insert(1);
+            set_lattice1.insert(42);
 
-    #[test]
-    fn merge_two_intersecting_sets() {
-        // let set1: HashSet<usize> = HashSet::new();
-        // let mut set_lattice1 = SetLattice(set1);
-        let mut set_lattice1: SetLattice<usize> = SetLattice::new();
-        set_lattice1.insert(1);
-        set_lattice1.insert(42);
+            let mut set_lattice2: OrderedSetLattice<usize> = OrderedSetLattice::new();
+            set_lattice2.insert(1);
+            set_lattice2.insert(100);
 
-        // let set2: HashSet<usize> = HashSet::new();
-        // let mut set_lattice2 = SetLattice(set2);
-        let mut set_lattice2: SetLattice<usize> = SetLattice::new();
-        set_lattice2.insert(1);
-        set_lattice2.insert(100);
+            assert_eq!(set_lattice1.intersect(&set_lattice2).size(), MaxLattice(1));
+        }
 
-        set_lattice1.do_merge(&set_lattice2);
+        #[test]
+        fn equality_of_two_ordered_sets() {
+            let mut set_lattice1: OrderedSetLattice<usize> = OrderedSetLattice::new();
+            set_lattice1.insert(1);
+            set_lattice1.insert(42);
 
-        assert_eq!(set_lattice1.size(), MaxLattice(3));
-    }
+            let mut set_lattice2: OrderedSetLattice<usize> = OrderedSetLattice::new();
+            set_lattice2.insert(1);
+            set_lattice2.insert(42);
 
-    #[test]
-    fn intersection_of_two_sets() {
-        // let set1: HashSet<usize> = HashSet::new();
-        // let mut set_lattice1 = SetLattice(set1);
-        let mut set_lattice1: SetLattice<usize> = SetLattice::new();
-        set_lattice1.insert(1);
-        set_lattice1.insert(42);
+            assert_eq!(set_lattice1, set_lattice2);
+        }
 
-        // let set2: HashSet<usize> = HashSet::new();
-        // let mut set_lattice2 = SetLattice(set2);
-        let mut set_lattice2: SetLattice<usize> = SetLattice::new();
-        set_lattice2.insert(1);
-        set_lattice2.insert(100);
+        #[test]
+        #[ignore]
+        fn inequality_of_two_ordered_sets() {
+            let mut set_lattice1: OrderedSetLattice<usize> = OrderedSetLattice::new();
+            set_lattice1.insert(1);
+            set_lattice1.insert(42);
 
-        assert_eq!(set_lattice1.intersect(&set_lattice2).size(), MaxLattice(1));
-    }
+            let mut set_lattice2: OrderedSetLattice<usize> = OrderedSetLattice::new();
+            set_lattice2.insert(42);
+            set_lattice2.insert(1);
 
-    #[test]
-    fn equality_of_two_sets() {
-        // let set1: HashSet<usize> = HashSet::new();
-        // let mut set_lattice1 = SetLattice(set1);
-        let mut set_lattice1: SetLattice<usize> = SetLattice::new();
-        set_lattice1.insert(1);
-        set_lattice1.insert(42);
-
-        // let set2: HashSet<usize> = HashSet::new();
-        // let mut set_lattice2 = SetLattice(set2);
-        let mut set_lattice2: SetLattice<usize> = SetLattice::new();
-        set_lattice2.insert(1);
-        set_lattice2.insert(42);
-
-        assert_eq!(set_lattice1, set_lattice2);
+            assert_ne!(set_lattice1, set_lattice2);
+        }
     }
 }
