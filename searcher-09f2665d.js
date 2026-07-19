2a00d01b8e28c59ca19134dde0573e5e0ea09992
andrewdavidmackenzie <andrew@mackenzie-serres.net>
2026-07-19 08:38:27 +0000

    Deploying to gh-pages from @ andrewdavidmackenzie/anna@8ff9e416e889f75d1c5dff563add0238d96faa31 🚀

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-40950dfe.js',
+                path_to_root + 'searchindex-d793c76d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
